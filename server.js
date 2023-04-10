3adc2528e104a6c64c157acbf8a6497cd394bb79
Hayden Davis <davishayden99@gmail.com>
2023-04-10 18:31:20 -0400

    first try

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 // Require http module
 
+const http = require('http');
+
 // Require fs module
+
+const fs = require('fs');
 
 // Require minimist module (make sure you install this one via npm).
 
+const mini = require('minimist');
+
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
+
+const args = mini(process.argv.slice(2));
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
+
+const port = args['port'] || 3000;
 
 // Use the fs module to create an arrow function using `fs.readFile`.
 // Use the documentation for the Node.js `fs` module. 
@@ -17,9 +27,17 @@
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
 
+const readIndexHtml = () => {
+  fs.readFile('./public/index.html', (err, data) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
 
-
-
+    // Do stuff with the file contents
+    console.log(data.toString());
+  });
+};
 
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
@@ -29,13 +47,24 @@
 // 3. end with the data that you are reading in from ./public/index.html.
 
 
-
-
+const server = http.createServer((req, res) => {
+  fs.readFile('./public/index.html', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    }
+  });
+});
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
 
-
-
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
 
 // That's it! You're all done!
